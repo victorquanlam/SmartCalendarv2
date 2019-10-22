e241b138e15db09d2c68678e17f17f3288be153b
ngoalex11 <101608489@student.swin.edu.au>
2019-10-22 19:02:25 +1100

    Revert "Merge branch 'master' of https://github.com/victorquanlam/SmartCalendarv2"
    This reverts commit 021e4bf307dbf6ed2f1285fe5d311c8960179518, reversing
    changes made to b0c712ebc2cc0d950b18a461010536267db73477.

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-manage',
+import { AuthService } from '../../shared/services/auth.service';
 })
 export class ManageComponent implements OnInit {
 
