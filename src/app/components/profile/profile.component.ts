c76b924e489f95ef95f82e59b381f1753acc10a3
QuanLam <37169648+hiendaica11145@users.noreply.github.com>
2019-11-07 02:03:29 +1100

    upgrade user page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { User } from 'src/app/user.model';
+import { UserService } from '../../user.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +11,60 @@ import { AuthService } from '../../shared/services/auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService,
+              public userService: UserService,
+              private formBuilder: FormBuilder) { }
 
+              boardsForm: FormGroup;
+  user: User;
+
+  isEditing=false;
+  uid='';
+  role='';
+  roleList =['Admin','Manager','User'];
   ngOnInit() {
+    this.boardsForm = this.formBuilder.group({
+      'displayName' : [null, Validators.required],
+      'email' : [null],
+      'firstName' : [null],
+      'lastName' : [null],
+      'role' : [null, Validators.required],
+      'uid' : [null, Validators.required],
+    });
+    this.boardsForm.disable()
+    
+    
+    
+  }
+
+  edit(id:string){
+    this.isEditing=true
+    if(id){
+      this.userService.getOneUser(id).subscribe(data => {
+        const tmp: any = data.payload.data();
+        if (tmp) {
+          this.uid=id;
+          this.role=tmp.role;
+          this.boardsForm.setValue({
+            displayName: tmp.displayName,
+            email: tmp.email,
+            firstName: tmp.firstName,
+            lastName: tmp.lastName,
+            role:tmp.role,
+            uid: id
+          });
+        }
+      })
+      this.boardsForm.enable()
+      this.boardsForm.controls['uid'].disable()
+      this.boardsForm.controls['role'].disable()
+    }
+  }
+
+
+  updateUser(){
+    this.isEditing=false
+    this.userService.updateUser(this.uid,{...this.boardsForm.value,role:this.role})
   }
 
 }
